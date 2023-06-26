4e2ae7ed90b790a662b3c8d49f6d1a1c84c549b7
SH8432 <wendysh8432@gamil.com>
2023-06-26 12:42:53 +0900

    FIX: add rds instance, change rds sg code

--- ChildManageSys/Terraform/security-group.tf
+++ ChildManageSys/Terraform/security-group.tf
@@ -14,25 +14,6 @@ resource "aws_security_group" "final-project-ecs-private-security-group" {
   }
 }
 
-# resource "aws_security_group" "rds-ecs-sg" {
-#   name = "rds-ecs-sg"
-#   vpc_id = aws_vpc.final-project-ecs-vpc.id
-  
-#   ingress {
-#     from_port = 3306
-#     to_port = 3306
-#     protocol = "tcp"
-#     security_groups = [aws_security_group.final-project-ecs-private-security-group.id]
-#   }
-
-#   egress {
-#     from_port = 0
-#     to_port = 0
-#     protocol = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-
 resource "aws_security_group_rule" "final-project-ecs-public-security-group-rule1" {
   type = "ingress"
   from_port = 22
@@ -69,22 +50,41 @@ resource "aws_security_group_rule" "final-project-ecs-public-security-group-outb
   security_group_id = aws_security_group.final-project-ecs-public-security-group.id
 }
 
-resource "aws_security_group_rule" "final-project-ecs-private-security-group-rule1" {
-  type = "ingress"
-  from_port = 3306
-  to_port = 3306
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.final-project-ecs-private-security-group.id
+/* Security Group for resources that want to access the Database */
+resource "aws_security_group" "db_access_sg" {
+  vpc_id      = aws_vpc.final-project-ecs-vpc.id
+  name        = "db-access-sg"
+  description = "Allow access to RDS"
 }
 
-resource "aws_security_group_rule" "final-project-ecs-private-security-group-outboundrule" {
-  type = "egress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.final-project-ecs-private-security-group.id
+resource "aws_security_group" "rds_sg" {
+  name = "rds-sg"
+  description = "final-project MYSQL DB Security Group"
+  vpc_id = aws_vpc.final-project-ecs-vpc.id
+
+  // allows traffic from the SG itself
+  ingress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      self = true
+  }
+
+  //allow traffic for TCP 5432
+  ingress {
+      from_port = 3306
+      to_port   = 3306
+      protocol  = "tcp"
+      security_groups = [aws_security_group.db_access_sg.id]
+  }
+
+  // outbound internet access
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "lb" {
