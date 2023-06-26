c4ec577adca4f8730adbad15dfad08338b8ed652
Sungah Park <sonia.0903.park@gmail.com>
2023-06-27 02:59:43 +0900

    FIX: variables.tf for env

--- ChildManageSys/Terraform/variables.tf
+++ ChildManageSys/Terraform/variables.tf
@@ -1,21 +1,25 @@
 variable "db_username" {
   type        = string
   description = "Username for the database"
+  default = "username"
 }
 
 variable "db_password" {
   type        = string
   description = "Password for the database"
+  default = "password"
 }
 
 variable "db_name" {
   type        = string
   description = "Name for the database"
+  default = "name"
 }
 
 variable "db_hostname" {
   type        = string
   description = "Hostname for the database"
+  default = "hostname"
 }
 
 variable "app_name" {
